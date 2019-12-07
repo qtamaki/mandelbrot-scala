ceaefd19e91ad4066902ed64157030c0986db1f8
Tatasunori TAMAKI <tamaki-t@ctws.jp>
2019-12-07 17:21:31 +0900

    update

---
+++ src/main/scala/mandelbrot/Main.scala
@@ -0,0 +1,5 @@
+package mandelbrot
+
+object Main extends App {
+  println("Hello")
+}
---
+++ src/test/scala/mandelbrot/MainSpec.scala
@@ -0,0 +1,8 @@
+package mandelbrot
+
+import org.scalatest._
+
+class MainSpec extends FlatSpec with Matchers {
+  "The Main object" should "say hello" in {
+  }
+}
