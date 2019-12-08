52d3a6337ce4dbd0018eed44a73a5cf612e7d5d4
Tatasunori TAMAKI <tamaki-t@ctws.jp>
2019-12-08 16:16:08 +0900

    Implementation of wandering algorithm

--- src/main/scala/mandelbrot/Main.scala
+++ src/main/scala/mandelbrot/Main.scala
@@ -2,6 +2,71 @@ package mandelbrot
 
 object Main extends App {
   println("Hello")
-  val m = Mandelbrot(200,50, Complex(-2,-2), Complex(2,2))
+  val m = Mandelbrot(50,10, Complex(-2,-2), Complex(2,2))
   Mandelbrot.printText(m)
+  println()
+
+  wandering(100, Wander(m, (0,0), (1.0/10)))
+
+  def move(n: Int, m: Mandelbrot): Unit = {
+    if (n == 0) return
+    Mandelbrot.printText(m)
+    println()
+    val (x,y) = m.center
+    move(n-1, m.moveCenter(x+1, y+1))
+  }
+
+  def zoom(n: Int, m: Mandelbrot): Unit = {
+    if (n == 0) return
+    Mandelbrot.printText(m)
+    println()
+    move(n-1, m.zoom(0.95))
+  }
+
+  def wandering(n: Int, w: Wander): Unit = {
+    if (n == 0) return
+    Mandelbrot.printText(w.m)
+    println()
+    println(s"m: ${w.m}, p: ${w.m.probe}")
+    wandering(n-1, w.next)
+  }
+}
+
+case class Wander(m: Mandelbrot, direction: (Int, Int), velocity: Double) {
+  def turn:(Int, Int) = {
+    val (x,y) = direction
+    (x * -1, y * -1)
+  }
+  def detect: (Int, Int) = {
+    val (x,y) = m.center
+    val l = List(-1, 0 ,1)
+    (for {
+      i <- l
+      j <- l
+    } yield {
+      (m.probe(x+i, y+j), (i, j))
+    }).minBy{case (x, (a,b)) => (x+9)%10}._2
+  }
+  def next: Wander = {
+      val d = detect
+      if (detect == (0,0)) {
+        println(s"zoom ${d}")
+        Wander(m = m.zoom(1-velocity), d, velocity)
+      }
+      else {
+        println(s"move ${d}")
+        Wander(m = moveMandelbrot(d), d, velocity)
+      }
+//    (probe, p) match {
+//      case (0, 0) => this.copy(m = moveMandelbrot)
+//      case (0, _) => Wander(m.zoom(1-velocity), turn, velocity, p)
+//      case (_, 0) => Wander(m.zoom(1-velocity), turn, velocity, p)
+//      case (_, _) => this.copy(m = moveMandelbrot)
+//    }
+  }
+
+  def moveMandelbrot(direction: (Int, Int)): Mandelbrot = {
+    val (x,y) = m.center
+    m.move(x+direction._1, y+direction._2)
+  }
 }
--- src/main/scala/mandelbrot/Mandelbrot.scala
+++ src/main/scala/mandelbrot/Mandelbrot.scala
@@ -3,7 +3,53 @@ package mandelbrot
 import scala.annotation.tailrec
 
 case class Mandelbrot(width: Int, height: Int, lowerComplex: Complex, distanceRe: Double, distanceIm: Double) {
+  val center:(Int, Int) = (width/2, height/2)
+
   def pointToComp(x: Int, y: Int): Complex = Mandelbrot.pointToComp(this, x, y)
+
+  def move(x: Int, y: Int): Mandelbrot = {
+    val (cx, cy) = center
+    val re_ = lowerComplex.re + distanceRe * (1.0 * (x - cx) / width)
+    val im_ = lowerComplex.im + distanceIm * (1.0 * (y - cy) / height)
+    copy(lowerComplex = Complex(re_, im_))
+  }
+
+  def moveCenter(x: Int, y: Int): Mandelbrot = {
+    val (cx, cy) = center
+    // センターとの距離x2分、範囲が小さくなる
+    val reScale = 1.0 * (width - Math.abs(cx - x) * 2) / width
+    val imScale = 1.0 * (height - Math.abs(cy - y) * 2) / height
+    // より小さくなる方に合わせてスケールする
+    val scale = Math.min(reScale, imScale)
+    // 中心とスケールを揃えてズームする
+    resize(x, y, scale)
+  }
+
+  def resize(x: Int, y: Int, scale: Double): Mandelbrot = {
+    // サイズから始点を割り出す
+    val lowX = x - (width * scale / 2).toInt
+    val lowY = y - (height * scale / 2).toInt
+    Mandelbrot(width, height, pointToComp(lowX, lowY), distanceRe * scale, distanceIm * scale)
+  }
+
+  def zoom(scale: Double): Mandelbrot = {
+    val (x,y) = center
+    resize(x,y, scale)
+  }
+
+  /**
+   * 現在の中心の状態を調べる
+   * @param m
+   * @return 0 - 9 で残回数が返る
+   */
+  def probe: Int = {
+    val (x,y) = center
+    probe(x,y)
+  }
+  def probe(x: Int, y: Int): Int = {
+    val c = pointToComp(x, y)
+    Mandelbrot.divergeTime(Complex.zero, c, 9)
+  }
 }
 
 object Mandelbrot {
@@ -27,7 +73,7 @@ object Mandelbrot {
    * テキストで表示させる
    * @param m
    */
-  def printText(m: Mandelbrot) {
+  def printText(m: Mandelbrot): Unit = {
     val z = Complex.zero
     for {
       y <- 0 until m.height
